e861109c93ef59eb0516752e30f2ac92e9ff3645
gongguhit <gonggu95@gmail.com>
2023-04-14 14:55:04 +0800

    basic cpp settings

---
+++ CStart/build/CMakeFiles/CStart.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CStart.dir/main.cpp.o"
+  "CMakeFiles/CStart.dir/main.cpp.o.d"
+  "CStart"
+  "CStart.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CStart.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CStart/build/CMakeFiles/CStart.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for CStart.
+# This may be replaced when dependencies are built.
